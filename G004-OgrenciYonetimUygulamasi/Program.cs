3f75fc05b31b604ae39cdca3fb0c43f27c8c08bb
Melih Budak <melihbudak@gmail.com>
2020-12-13 17:21:54 +0300

    Add project files.

---
+++ G004-OgrenciYonetimUygulamasi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G004_OgrenciYonetimUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
